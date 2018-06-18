735405e49a0bdf7e1f30540cdd622b7629e987a2
jon-may <jon.may@plymouth.ac.uk>
2018-06-18 11:54:42 +0100

    Add files via upload

---
+++ Analysis_S1.R
@@ -0,0 +1,152 @@
+### Analysis of Study 1 using MTFSMPA.csv
+### NB set your working directory to the folder containing this script and the data file
+# setwd("/R_Analysis")
+
+## load packages
+library(readr)
+library("lavaan")
+library("semTools")
+
+## CFA anlayses are long so increase maximum number of lines to print
+options(max.print = 99999)
+
+## read data file
+MTFPA1 <- read.csv("MTFSMPA.csv")
+
+## create output file for analysis to be sent to
+sink('MTFAnalysis.txt', split='TRUE')
+
+fit.scaled<-c("chisq.scaled", "df", "cfi.robust", "tli.robust", "aic", "bic", "srmr", "srmr_bentler", "rmsea.robust","rmsea.ci.lower.robust","rmsea.ci.upper.robust")
+
+
+## define three models to compare
+## Unifactorial model as a baseline
+MTF.model1 <- 'intensity =~ WANTF + NEEDF + URGEF + 
+            GOODF + BETTERF + WORSEF + DOINGF + HOWF + SUCCEEDF + PASTF + 
+            THOUGHTF + REMINDF + GRABF'
+
+## three factor model as in Robinson et al. (2016)
+MTF.model3 <- 'intensity =~ WANTF + NEEDF + URGEF 
+             imagery =~ GOODF + BETTERF + WORSEF + DOINGF + HOWF + SUCCEEDF + PASTF
+             availability =~ THOUGHTF + REMINDF + GRABF'
+
+## four factor model as in Parham et al (2016) and Kavanagh et al (2018)
+MTF.model4 <- 'intensity =~ WANTF + NEEDF + URGEF
+               imageryi =~ GOODF + BETTERF + WORSEF 
+              imageryse =~ DOINGF + HOWF + SUCCEEDF + PASTF
+            availability =~ THOUGHTF + REMINDF + GRABF'
+
+## fit the Unifactorial model
+
+fit <- cfa(MTF.model1, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+
+
+## fit the 3 Factor model
+fit <- cfa(MTF.model3, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+
+## fit the 4 Factor model
+fit <- cfa(MTF.model4, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+summary(fit, fit.measures=TRUE, modindices=TRUE)
+reliability(fit)
+
+## iteratively add highest MI error covariance pair within factor
+MTF.model4e1 <- paste(MTF.model4,"\nDOINGF ~~ HOWF")
+fit <- cfa(MTF.model4e1, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+reliability(fit)
+
+MTF.model4e2 <- paste(MTF.model4e1,'\n WANTF ~~ URGEF')
+fit <- cfa(MTF.model4e2, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+reliability(fit)
+
+
+MTF.model4e3 <- paste(MTF.model4e2,'\n REMINDF ~~ GRABF')
+fit <- cfa(MTF.model4e3, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+reliability(fit)
+
+fitMeasures(fit, fit.scaled)
+
+MTF.model4e4 <- paste(MTF.model4e3,'\n GOODF ~~ WORSEF')
+fit <- cfa(MTF.model4e4, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+reliability(fit)
+
+MTF.model4e5 <- paste(MTF.model4e4,'\n SUCCEEDF ~~ PASTF')
+fit <- cfa(MTF.model4e5, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+reliability(fit)
+
+## return to model4 to compare invariances over site
+fit <- cfa(MTF.model4, data=MTFPA1, estimator="MLR", group = "Site") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+measurementInvariance(MTF.model4, data=MTFPA1, estimator="MLR", group = "Site")
+reliability(fit)
+
+
+## try again with the four EV added
+fit <- cfa(MTF.model4e4, data=MTFPA1, estimator="MLR", group = "Site") 
+fitMeasures(fit, fit.scaled)
+summary(fit, fit.measures=TRUE, modindices=TRUE)
+measurementInvariance(MTF.model4e4, data=MTFPA1, estimator="MLR", group = "Site")
+reliability(fit)
+
+## close output file
+sink()
+
+### Analyses for SM
+
+## open output file
+sink('SMAnalysis.txt',split=TRUE)
+
+## One factor for baseline
+SMS.model1a <- 'intensity =~ WANT + NEED + URGE + 
+            GOOD + BETTER + WORSE + DOING + HOW + SUCCEED + PAST + 
+            REMIND + GRAB '
+fit <- cfa(SMS.model1a, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+
+
+## 3 factor as in Robinson et al. (2016)
+SMS.model3a <- 'intensity =~ WANT + NEED + URGE 
+                imagery =~ GOOD + BETTER + WORSE + DOING + HOW + SUCCEED + PAST
+                 availability =~ REMIND + GRAB'
+fit <- cfa(SMS.model3a, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+
+
+## 4 factor as in Parham (2016), Kavanagh (2018)
+SMS.model4a <- 'intensity =~ WANT + NEED + URGE
+                imageryi =~ GOOD + BETTER + WORSE 
+                imageryse =~ DOING + HOW + SUCCEED + PAST
+                availability =~ REMIND + GRAB'
+fit <- cfa(SMS.model4a, data=MTFPA1, estimator="MLR") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+reliability(fit)
+
+## check invariance over sites
+fit <- cfa(SMS.model4a, data= MTFPA1, estimator="MLR", group = "Site") 
+fitMeasures(fit, fit.scaled)
+#summary(fit, fit.measures=TRUE, modindices=TRUE)
+measurementInvariance(SMS.model4a, data= MTFPA1, estimator="MLR", group = "Site")
+reliability(fit) 
+
+sink()
+
+
+
